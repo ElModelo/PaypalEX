623af212fd12199101df776a091152c532c648c4
Fernando Cornielle <Fernandocornielle@gmail.com>
2019-01-31 22:59:52 -0400

    instanciando las clases y usando los metodos para los articulos a pagar

--- pagar.php
+++ pagar.php
@@ -4,7 +4,9 @@ if (!isset($_POST['producto'], $_POST['precio'])) {
 	exit("Hubo un error");
 }
 
-
+// namespace para poder importar las clases de otros archivos
+use PayPal\Api\Payer;
+use PayPal\Api\Item;
 
 require 'config.php';
 
@@ -13,3 +15,14 @@ $precio = htmlspecialchars($_POST['precio']);
 $precio = (int) $precio;
 $envio = 0;
 $total = $precio + $envio;
+
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+
+$articulo = new Item();
+$articulo->setName($producto)
+		 ->setCurrency('USD')
+		 ->setQuantity(1)
+		 ->setPrice($precio);
+
+echo $articulo->getQuantity();
